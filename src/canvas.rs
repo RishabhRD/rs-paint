eec0d15e8d5cb9d760b3cd765a1ccb107ae67c36
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-02-15 19:32:53 +0530

    updated canvas

--- src/canvas.rs
+++ src/canvas.rs
@@ -3,16 +3,18 @@
 
 use crate::{color::Color, document::Document};
 
+/// # Class Invariant
+///   - m_history.len() >= 1
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct DrawingCanvas {
-    /// Document that decides what would be saved in disk after on save.
-    m_final_doc: Document,
-    /// Document that is visible on screen.
+    /// History of document states.
+    m_history: Vec<Document>,
+
+    /// Currently selected document index.
+    m_selected: usize,
+
+    /// Visible document to render.
     m_visible_doc: Document,
-    /// Undo stack of current canvas.
-    m_undo_stack: Vec<Document>,
-    /// Redo stack of current canvas.
-    m_redo_stack: Vec<Document>,
 }
 
 impl DrawingCanvas {
@@ -20,57 +22,39 @@ impl DrawingCanvas {
     pub fn new(width: usize, height: usize, bg_color: Color) -> Self {
         let doc = Document::new(width, height, bg_color);
         DrawingCanvas {
-            m_final_doc: doc.clone(),
+            m_history: vec![doc.clone()],
+            m_selected: 0,
             m_visible_doc: doc,
-            m_undo_stack: vec![],
-            m_redo_stack: vec![],
         }
     }
 
-    /// Commits visible document as final document.
-    ///
-    /// # Postcondition
-    ///   - Makes visible document as final document that can be saved in disk.
-    ///   - Clears the redo stack.
-    pub fn commit_visible(&mut self) {
-        self.m_final_doc = self.m_visible_doc.clone();
-        self.m_redo_stack.clear();
+    /// Removes all the document after currently selected document in history
+    /// and the add visible document to history.
+    pub fn commit(&mut self) {
+        self.m_history.truncate(self.m_selected + 1);
+        self.m_history.push(self.m_visible_doc.clone());
     }
 
-    /// Replaces final and visible document with last undo state.
-    ///
-    /// # Postcondition
-    ///   - If m_undo_stack is empty, do nothing.
-    ///   - m_final_doc is replaced with last undo state.
-    ///   - m_visible_doc is replaced with last undo state.
-    ///   - add last undo state to redo state.
-    pub fn undo(&mut self) {
-        match self.m_undo_stack.pop() {
-            None => (),
-            Some(undo_doc) => {
-                self.m_final_doc = undo_doc.clone();
-                self.m_visible_doc = undo_doc.clone();
-                self.m_redo_stack.push(undo_doc.clone());
-            }
+    /// Undo the last operation and changes visible document to currently selected state.
+    /// Returns true if there is something to undo otherwise returns false.
+    pub fn undo(&mut self) -> bool {
+        if self.m_selected == 0 {
+            return false;
         }
+        self.m_selected -= 1;
+        self.m_visible_doc = self.m_history[self.m_selected].clone();
+        true
     }
 
-    /// Replaces final and visible document with last redo state.
-    ///
-    /// # Postcondition
-    ///   - If m_redo_stack is empty, do nothing.
-    ///   - m_final_doc is replaced with last redo state.
-    ///   - m_visible_doc is replaced with last redo state.
-    ///   - add last redo state to undo state.
-    pub fn redo(&mut self) {
-        match self.m_redo_stack.pop() {
-            None => (),
-            Some(redo_doc) => {
-                self.m_final_doc = redo_doc.clone();
-                self.m_visible_doc = redo_doc.clone();
-                self.m_undo_stack.push(redo_doc.clone());
-            }
+    /// Redo the last undo operation and changes visible document to currently selected state.
+    /// Returns true if there is something to redo otherwise returns false.
+    pub fn redo(&mut self) -> bool {
+        if self.m_selected == self.m_history.len() - 1 {
+            return false;
         }
+        self.m_selected += 1;
+        self.m_visible_doc = self.m_history[self.m_selected].clone();
+        true
     }
 
     /// Returns reference to visible document.
@@ -83,8 +67,40 @@ impl DrawingCanvas {
         &mut self.m_visible_doc
     }
 
-    /// Returns reference to final document.
-    pub fn final_doc(&self) -> &Document {
-        &self.m_final_doc
+    /// Returns reference to history of document.
+    pub fn history(&self) -> &Vec<Document> {
+        &self.m_history
+    }
+
+    /// Returns index of current state of document in history.
+    pub fn current_idx(&self) -> usize {
+        self.m_selected
+    }
+
+    /// Returns index of current state of document in history.
+    pub fn current(&self) -> &Document {
+        &self.m_history[self.m_selected]
+    }
+
+    /// Clear history from given index.
+    ///
+    /// # Precondition
+    ///   - i < self.hisotry().len()
+    ///   - i > 0
+    pub fn clear_history_from(&mut self, i: usize) {
+        self.m_history.truncate(i + 1);
+        self.m_selected = i;
+    }
+
+    /// Erases history element at given index.
+    ///
+    /// # Precondition
+    ///   - i < self.history().len()
+    ///   - self.history().len() > 1
+    pub fn erase_history_element(&mut self, i: usize) {
+        self.m_history.remove(i);
+        if i <= self.m_selected {
+            self.m_selected -= 1;
+        }
     }
 }
